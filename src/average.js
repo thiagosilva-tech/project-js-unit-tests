177f6ed461f367a1dca60ff4a57f435a13b9c79d
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-06 11:02:50 -0300

    feat: requisito 01

--- src/average.js
+++ src/average.js
@@ -11,8 +11,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  let soma = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'number') {
+      soma += array[index];
+    } else {
+      return undefined;
+    }
+  }
+  let media = Math.round(soma / array.length);
+  return media;
 };
 
 module.exports = average;
